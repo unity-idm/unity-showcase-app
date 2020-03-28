cbea34399b90f38fe8cac011a0fa0238782d48bd
Roman Krysinski <rkrysinski@gmail.com>
2020-03-28 07:58:39 +0100

    Finishing notes view, adding mock screens for the rest of views.

--- src/main/java/io/imunity/cloud/showcase/MVCController.java
+++ src/main/java/io/imunity/cloud/showcase/MVCController.java
@@ -1,26 +1,80 @@
 package io.imunity.cloud.showcase;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MVCController
 {
+	public enum NavigationItem
+	{
+		NOTES, USER_PROFILE, BILLINGS, USERS, NEW_NOTEBOOK
+	}
+
 	@RequestMapping("/")
 	public String main()
 	{
 		return "pricing";
 	}
-	
+
 	@RequestMapping("/tenant-selection")
 	public String selectTenant()
 	{
 		return "tenant-selection";
 	}
-	
+
 	@RequestMapping("/application")
-	public String application()
+	public String application(Model model)
 	{
-		return "application";
+		setNavigationItem(model, NavigationItem.NOTES);
+		return "application/notes";
+	}
+
+	@RequestMapping("/application/notes")
+	public String applicationNotes(Model model)
+	{
+		setNavigationItem(model, NavigationItem.NOTES);
+		return "application/notes";
+	}
+
+	@RequestMapping("/application/notes/new")
+	public String applicationNotesNew(Model model)
+	{
+		setNavigationItem(model, NavigationItem.NOTES);
+		return "application/notes_new";
+	}
+
+	@RequestMapping("/application/user")
+	public String applicationUser(Model model)
+	{
+		setNavigationItem(model, NavigationItem.USER_PROFILE);
+		return "application/user";
+	}
+	
+	@RequestMapping("/application/billings")
+	public String applicationBillings(Model model)
+	{
+		setNavigationItem(model, NavigationItem.BILLINGS);
+		return "application/billings";
+	}
+	
+	@RequestMapping("/application/users")
+	public String applicationUsers(Model model)
+	{
+		setNavigationItem(model, NavigationItem.USERS);
+		return "application/users";
+	}
+	
+	@RequestMapping("/application/new")
+	public String applicationNew(Model model)
+	{
+		setNavigationItem(model, NavigationItem.NEW_NOTEBOOK);
+		return "application/new_notebook";
+	}
+
+	private void setNavigationItem(Model model, NavigationItem selected)
+	{
+		model.addAttribute("selected_nav", selected);
 	}
 }
